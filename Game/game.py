131b20d5c4f4401d94726e703e8ab4420db7a795
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-03-05 10:23:59 +0530

    Finished up the Game and CLI setup

--- Game/game.py
+++ Game/game.py
@@ -6,8 +6,11 @@ class Bowling_Game:
     def __init__(self, score_calc):
         self._roll_parser = Roll_Parser()
         self._frame_parser = Frame_Parser()
+        self.score_calculator = score_calc
     
     def score_of_game(self, input):
         parsed_roll = self._roll_parser.parse(input)
         parsed_frame = self._frame_parser.parse(parsed_roll)
+        score = self.score_calculator.calc_score(parsed_frame)
+        return score
 
--- main.py
+++ main.py
@@ -1,14 +1,17 @@
+from Game.player import Player
+from Game.game import Bowling_Game
+from Game.score_calc import Score_Calculator
 from constants import STRIKE_SYMBOL, SPARE_SYMBOL , MISS_SYMBOL
 
 def declare_winner(winner):
-    print()
+    print(f'{winner.get_name()} has won the game with {winner.get_final_score()}')
 
 def display_scorecard(players):
     print()
     print('-- ScoreCard ---')
     print()
     for idx, player in enumerate(players):
-        print()
+        print( f" {idx + 1} - {player.get_name()}  scored  {player.get_final_score()}")
 
 if __name__ == '__main__':
     score_calc = Score_Calculator()
@@ -26,5 +29,13 @@ if __name__ == '__main__':
     print()
     print(f"** Use {STRIKE_SYMBOL} for STRIKE, {SPARE_SYMBOL} for SPARE , {MISS_SYMBOL} for a MISS **")
     print()
-
+    for player in players:
+        player_frame = input(f"Enter {player.get_name()} bowling frame: ")
+        player_score = bowling_game.score_of_game(player_frame)
+        player.set_final_score(player_score)
+    
+    players.sort(key=lambda player: player.get_final_score(), reverse=True)
+    
+    declare_winner(players[0])
+    display_scorecard(players)
 
