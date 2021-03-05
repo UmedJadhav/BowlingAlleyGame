b3cf11ce1de1e95fcd25919678426b9fd1e037d7
Umed Jadhav <umed.jadhav@somaiya.edu>
2021-03-05 10:22:26 +0530

    Created Frame Parser to convert player rolls to frames

--- Game/game.py
+++ Game/game.py
@@ -1,12 +1,13 @@
+from Frame.frame_parser import Frame_Parser
 from Roll.roll_parser import Roll_Parser
 
 
 class Bowling_Game:
     def __init__(self, score_calc):
         self._roll_parser = Roll_Parser()
-
+        self._frame_parser = Frame_Parser()
     
     def score_of_game(self, input):
         parsed_roll = self._roll_parser.parse(input)
-
+        parsed_frame = self._frame_parser.parse(parsed_roll)
 
